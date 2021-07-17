2d4d5891fb3b2e06dff8a58341c6b312cdd8656a
Rjayaprakashnarayana <jayaprakashrayani@gmail.com>
2021-07-17 16:24:30 +0530

    Create 2.3_equnotequ.py

---
+++ 2.3_equnotequ.py
@@ -0,0 +1,6 @@
+a = int(input("Enter value of a variable:"))
+b = int(input("Enter value of b variable:"))
+if(a==b): #here I full filled the need and aim of the program
+    print("Both values are same")
+elif(a!=b): #here I full filled the need and aim of the program
+    print("Both values are different")
