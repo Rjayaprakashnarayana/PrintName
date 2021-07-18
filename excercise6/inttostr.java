617f86b820043d365fd263385c3fa9b59145755c
Rjayaprakashnarayana <jayaprakashrayani@gmail.com>
2021-07-18 16:37:17 +0530

    Create inttostr.java

---
+++ excercise6/inttostr.java
@@ -0,0 +1,10 @@
+public class inttostr{
+  public static void main(String[] args){
+    
+    Integer i = new Integer(123);
+
+    System.out.println("Before conversion: " + i.getClass().getName());
+    
+    System.out.println("After conversion: " + String.valueOf(i).getClass().getName());
+  }
+}
