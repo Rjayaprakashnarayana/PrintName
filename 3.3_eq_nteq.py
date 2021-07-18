451644c7fd3f1eee9828334cfb8c95f9090cfdc3
Rjayaprakashnarayana <jayaprakashrayani@gmail.com>
2021-07-18 14:34:32 +0530

    Delete 3.3_eq_nteq.py

--- 3.3_eq_nteq.py
+++
@@ -1,6 +0,0 @@
-a = int(input("Enter value of a variable:"))
-b = int(input("Enter value of b variable:"))
-if(a==b): #here I full filled the need and aim of the program
-    print("Both variables are equal")
-elif(a!=b): #here I full filled the need and aim of the program
-    print("Both variables are not equal")
