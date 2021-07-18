b5496d8f1cad5373881fe54581fd310393851858
Rjayaprakashnarayana <jayaprakashrayani@gmail.com>
2021-07-18 14:42:17 +0530

    Delete comments.py

--- comments.py
+++
@@ -1,7 +0,0 @@
-def comment_1():
-    """This is to represent the
-    multi line comments as well as documentation"""
-    print("Sucessfully executed")
-
-#single line comment
-print(comment_1.__doc__)
