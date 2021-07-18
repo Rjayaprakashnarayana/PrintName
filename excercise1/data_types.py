fa27e573254b06c7d2ca03ac9146421da09844a8
Rjayaprakashnarayana <jayaprakashrayani@gmail.com>
2021-07-18 14:44:25 +0530

    Create data_types.py

---
+++ excercise1/data_types.py
@@ -0,0 +1,8 @@
+a= int('1') #integer data type
+b= bool('True')#boolean data type
+c = str('J') #string data type
+d = float('1.413') #In python the range of float is limit less , so there will be no doubele
+print(a) #display_the_values
+print(b)
+print(c)
+print(d)
