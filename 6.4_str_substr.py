3a5cfe8288d5fa75521f054f8e51680520e21f9d
Rjayaprakashnarayana <jayaprakashrayani@gmail.com>
2021-07-18 14:41:55 +0530

    Delete 6.4_str_substr.py

--- 6.4_str_substr.py
+++
@@ -1,5 +0,0 @@
-s1 = input("Enter a string:")
-begin=int(input("Enter the position begin for string:"))
-end=int(input("Enter the position end for string:"))
-extract_1 = s1[begin:end]
-print(extract_1)
