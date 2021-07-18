13adc13d5545b5915d2f90c5280a4279e0e85eec
Rjayaprakashnarayana <jayaprakashrayani@gmail.com>
2021-07-18 14:24:11 +0530

    Create 2.6_fun_relational.py

---
+++ excercise2/2.6_fun_relational.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a value:"))
+b = int(input("Enter b value:"))
+if (a<b):
+    print("a value is lessthan b")
+elif (a>b):
+    print("a value is greaterthan b")
+elif(a<=b):
+    print("a value is lessthan or equalto b")
+elif(a>=b):
+    print("a value is greaterthan or equalto b")
