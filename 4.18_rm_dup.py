fb14aee15116cb8cb7720ecba7454056a150abc6
Rjayaprakashnarayana <jayaprakashrayani@gmail.com>
2021-07-18 14:36:34 +0530

    Delete 4.18_rm_dup.py

--- 4.18_rm_dup.py
+++
@@ -1,7 +0,0 @@
-my_list = []
-n = int(input("Enter Number of Elements in array:"))
-for i in range(n):
-    b= int(input())
-    my_list.insert(i,b)
-my_finallist = [i for j, i in enumerate(my_list) if i not in my_list[:j]] 
-print(list(my_finallist))
