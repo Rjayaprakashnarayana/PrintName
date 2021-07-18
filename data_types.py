17bb5ea4df931cc925cae49fad4c6726a2164ab8
Rjayaprakashnarayana <jayaprakashrayani@gmail.com>
2021-07-18 14:42:22 +0530

    Delete data_types.py

--- data_types.py
+++
@@ -1,8 +0,0 @@
-a= int('1') #integer data type
-b= bool('True')#boolean data type
-c = str('J') #string data type
-d = float('1.413') #In python the range of float is limit less , so there will be no doubele
-print(a) #display_the_values
-print(b)
-print(c)
-print(d)
