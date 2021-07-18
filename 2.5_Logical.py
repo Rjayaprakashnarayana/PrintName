1b8513b06d02c79fc294524f42901fdaa0e8e181
Rjayaprakashnarayana <jayaprakashrayani@gmail.com>
2021-07-18 14:22:47 +0530

    Delete 2.5_Logical.py

--- 2.5_Logical.py
+++
@@ -1,8 +0,0 @@
-a= int(input("enter a value:"))
-b= int(input("enter a value:"))
-if(a not in [1,3,4]):#used not keyword
-    print("a out of range")
-elif(a==1 and b==2):#used and keyword
-    print("Expected values")
-elif(a==10 or b==12):#used or keyword
-    print("One or both conditions satisfied")
