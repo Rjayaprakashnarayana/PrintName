76d6e61aed101881d8e730cb08ff8bb09883f155
Rjayaprakashnarayana <jayaprakashrayani@gmail.com>
2021-07-19 15:15:52 +0530

    Update OddEvenInArrayExample.java

--- exercise3/OddEvenInArrayExample.java
+++ exercise3/OddEvenInArrayExample.java
@@ -5,7 +5,7 @@ System.out.println("Odd Numbers:");
 for(int i=0;i<a.length;i++){  
 if(a[i]%2!=0){  
 System.out.println(a[i]);  
-}  
+}   
 }  
 System.out.println("Even Numbers:");  
 for(int i=0;i<a.length;i++){  
