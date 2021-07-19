b3eea4a4483f39ad12d14c9ce1e9fbc8deac01f9
Rjayaprakashnarayana <jayaprakashrayani@gmail.com>
2021-07-19 06:56:42 +0530

    Create run.java

---
+++ ecercise9/run.java
@@ -0,0 +1,32 @@
+public abstract class Test {
+
+    void t1()
+    {
+        System.out.println("super");
+
+    }
+
+}
+ public class concret extends Test{
+
+    void t1()
+{
+    super.t1(); // First call the superclass implementation
+    System.out.println("child");
+}
+    void t2()
+    {
+        System.out.println("child2");
+
+    }
+
+}
+
+public class run {
+    public static void main(String[] args) {
+        Test t=new concret();
+
+        t.t1();
+    }
+
+}
